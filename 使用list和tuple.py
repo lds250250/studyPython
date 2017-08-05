943a12a991a81f13165ca49e6bf0ea6f4074dacb
多云 <30614302+lds250250@users.noreply.github.com>
2017-08-05 10:31:37 +0800

    Update 使用list和tuple.py

--- 使用list和tuple.py
+++ 使用list和tuple.py
@@ -1,3 +1,7 @@
+# 请用索引取出下面list的指定元素：
+# 打印Apple:
+# 打印Python:
+# 打印Lisa:
 L = [['Apple', 'Google', 'Microsoft'], ['Java', 'Python', 'Ruby', 'PHP'],
      ['Adam', 'Bart', 'Lisa']]
 print(L[0][0], L[1][1], L[2][2], sep='\n')
