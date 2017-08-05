d2ddf984c426a98783d165a6cb071996fc795625
多云 <30614302+lds250250@users.noreply.github.com>
2017-08-05 12:09:39 +0800

    Create 定义函数.py

---
+++ 定义函数.py
@@ -0,0 +1,14 @@
+# 请定义一个函数quadratic(a, b, c)，接收3个参数，返回一元二次方程：
+# ax2 + bx + c = 0的两个解。
+# 提示：计算平方根可以调用math.sqrt()函数：
+import math
+
+
+def quadratic(a, b, c):
+    return (-b + math.sqrt(b * b - 4 * a * c)) / 2 / a, (
+        -b - math.sqrt(b * b - 4 * a * c)) / 2 / a
+
+
+# 测试:
+print(quadratic(2, 3, 1))  # => (-0.5, -1.0)
+print(quadratic(1, 3, -4))  # => (1.0, -4.0)
